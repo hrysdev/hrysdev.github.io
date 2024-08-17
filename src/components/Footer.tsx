d4138b66899e3e2ad09519d38bfbbea0eada0323
hrysdev <hrysdev@gmail.com>
2024-08-18 04:50:39 +0900

    feat: フッターを追加
    Signed-off-by: hrysdev <hrysdev@gmail.com>

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+import Typography from "@mui/material/Typography";
+
+export default function Footer() {
+  return (
+    <Typography textAlign="center" sx={{ my: 3 }}>
+      &copy; 2024 HrysDev.
+    </Typography>
+  );
+}
