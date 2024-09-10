cab6ae9295c3b668fa0712a92fd5f62db1571e31
hrysdev <hrysdev@gmail.com>
2024-09-10 22:25:39 +0900

    feat: フッターの文字サイズを変更
    Signed-off-by: hrysdev <hrysdev@gmail.com>

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import Typography from "@mui/material/Typography";
 
 export default function Footer() {
   return (
-    <Typography textAlign="center" sx={{ my: 4 }}>
+    <Typography variant="h6" textAlign="center" sx={{ my: 11 }}>
       &copy; 2024 HrysDev.
     </Typography>
   );
