ac844d8b73e14a76a3c4cdf9babc872f207601af
hrysdev <hrysdev@gmail.com>
2024-09-16 02:04:11 +0900

    feat: 仮で各種リンクボタンを追加
    Signed-off-by: hrysdev <hrysdev@gmail.com>

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -6,6 +6,10 @@ import Card from "@mui/material/Card";
 import CardMedia from "@mui/material/CardMedia";
 import CardContent from "@mui/material/CardContent";
 import CardActionArea from "@mui/material/CardActionArea";
+import XIcon from "@mui/icons-material/X";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import IconButton from "@mui/material/IconButton";
+import Box from "@mui/material/Box";
 
 const works = [
   {
@@ -14,13 +18,13 @@ const works = [
     imgPath: "./320x320.png",
   },
   {
-    title: "dummy title",
-    date: "2024/09/11",
+    title: "プロジェクトタイトル",
+    date: "xxxx/xx/xx",
     imgPath: "./320x320.png",
   },
   {
-    title: "dummy title",
-    date: "2024/09/11",
+    title: "プロジェクトタイトル",
+    date: "xxxx/xx/xx",
     imgPath: "./320x320.png",
   },
 ];
@@ -28,14 +32,28 @@ const works = [
 export default function Main() {
   return (
     <Container maxWidth="lg">
-      <Stack direction="column" spacing={3} textAlign="center" sx={{ my: 9 }}>
-        <Typography variant="h4" fontWeight="bold" letterSpacing={2}>
-          HrysDev's Works
-        </Typography>
-        <Typography letterSpacing={1}>
-          ここは、趣味で行っている個人開発をまとめたポートフォリオサイトです。
-        </Typography>
-      </Stack>
+      <Box sx={{ my: 8, userSelect: "none" }}>
+        <Stack direction="column" spacing={2} textAlign="center">
+          <Typography variant="h4" fontWeight="bold" letterSpacing={2}>
+            HrysDev's Works
+          </Typography>
+          <Typography letterSpacing={1}>
+            ここは、趣味で行っている個人開発をまとめたポートフォリオサイトです。
+          </Typography>
+        </Stack>
+        <Stack
+          direction="row"
+          spacing={1}
+          sx={{ display: "flex", justifyContent: "center", mt: 4 }}
+        >
+          <IconButton size="large">
+            <XIcon />
+          </IconButton>
+          <IconButton size="large">
+            <GitHubIcon />
+          </IconButton>
+        </Stack>
+      </Box>
       <Grid container spacing={{ xs: 3, md: 4 }}>
         {works.map((work, i) => {
           return (
@@ -53,7 +71,7 @@ export default function Main() {
                       pl: 4,
                       pb: 4,
                       bgcolor: "rgba(0, 0, 0, 0.6)",
-                      opacity: 1,
+                      opacity: 0,
                       transition: "opacity 0.3s",
                       "&:hover": {
                         opacity: 1,
