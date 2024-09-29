9531b805fc9a80d40f18ceb833d0760782ef8b2a
hrysdev <hrysdev@gmail.com>
2024-09-29 20:15:42 +0900

    feat: 常にページの一番下に配置されるように修正
    Signed-off-by: hrysdev <hrysdev@gmail.com>

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,9 +4,15 @@ export default function Footer() {
   return (
     <Typography
       variant="h6"
-      color="text.secondary"
-      textAlign="center"
-      sx={{ mt: 15, py: 3 }}
+      sx={{
+        position: "fixed",
+        bottom: 0,
+        width: "100%",
+        color: "text.secondary",
+        textAlign: "center",
+        mt: 15,
+        py: 2,
+      }}
     >
       &copy; 2024 HrysDev.
     </Typography>
